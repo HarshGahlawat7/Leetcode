229c8a486f3c3172543349b51a9224cf51495f35
HarshGahlawat7 <gahlawatharsh69@gmail.com>
2025-01-03 21:35:21 +0530

    Time: 3 ms (64.36%), Space: 8.1 MB (10.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,4 @@
 class Solution {
 public:
-    int fib(int n) {
-        if (n <= 1)
-          return n;
-        return fib(n - 1) + fib(n - 2);
-        }
+    int fib(int n) { if (n <= 1) return n; std::vector<int> fib_seq(n + 1);  fib_seq[0] = 0; fib_seq[1] = 1; for (int i = 2; i <= n; ++i) { fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2]; } return fib_seq[n]; }
 };
