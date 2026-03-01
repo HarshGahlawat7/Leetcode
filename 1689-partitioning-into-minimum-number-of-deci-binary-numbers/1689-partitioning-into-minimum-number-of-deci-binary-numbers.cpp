8bb555f0f134d2a81383b5c02e3b170bc35ebcd8
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-01 23:54:10 +0530

    Time: 0 ms (100%), Space: 16.5 MB (81.74%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -3,7 +3,9 @@ public:
     int minPartitions(string n) {
         char max_digit = '0';
     for (char c : n) {
-        if (c > max_digit) max_digit = c;
+        if (c > max_digit) {
+            max_digit = c;
+        }
     }
     return max_digit - '0';
     }
