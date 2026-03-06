21d812e8eec5e381e089e527a36ecf5ce6ca2fc7
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-06 22:39:09 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.47%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        for (int i = 1; i < s.length(); i++) {
-            // If we find a '1' that follows a '0', 
-            // a new segment has started.
-            if (s[i-1] == '0' && s[i] == '1') {
-                return false;
-            }
-        }
-        return true;
+        return s.find("01") == string::npos;
     }
 };
