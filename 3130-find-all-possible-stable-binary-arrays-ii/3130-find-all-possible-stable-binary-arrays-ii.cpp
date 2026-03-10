09d4cc2c6f002cff0ca0645899aa81dc890d4f2c
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-10 21:00:36 +0530

    Time: 150 ms (67.12%), Space: 176.9 MB (49.31%) - LeetHub

--- 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
+++ 3130-find-all-possible-stable-binary-arrays-ii/3130-find-all-possible-stable-binary-arrays-ii.cpp
@@ -3,40 +3,38 @@ public:
     int numberOfStableArrays(int zero, int one, int limit) {
         long long MOD = 1e9 + 7;
         
-        // dp[i][j][k]
-        // i: number of 0s used
-        // j: number of 1s used
-        // k: 0 if the last digit is 0, 1 if the last digit is 1
-        vector<vector<vector<long long>>> dp(zero + 1, vector<vector<long long>>(one + 1, vector<long long>(2, 0)));
+        // We use two 2D arrays to represent the current number of 1s used (j)
+        // and whether the last digit was 0 or 1.
+        // dp[i][j][0] is now dp0[i][j]
+        // dp[i][j][1] is now dp1[i][j]
+        vector<vector<long long>> dp0(zero + 1, vector<long long>(one + 1, 0));
+        vector<vector<long long>> dp1(zero + 1, vector<long long>(one + 1, 0));
 
-        // Base cases: filling arrays with only 0s or only 1s up to the limit
+        // Base Case: Only zeros
         for (int i = 1; i <= min(zero, limit); ++i) {
-            dp[i][0][0] = 1;
+            dp0[i][0] = 1;
         }
+        // Base Case: Only ones
         for (int j = 1; j <= min(one, limit); ++j) {
-            dp[0][j][1] = 1;
+            dp1[0][j] = 1;
         }
 
         for (int i = 1; i <= zero; ++i) {
             for (int j = 1; j <= one; ++j) {
-                // To end with a 0 at (i, j):
-                // We take all stable arrays of (i-1, j) ending in 0 or 1
-                // then subtract the invalid case where we just exceeded the 'limit' of 0s.
-                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+                // Transition for ending in 0
+                dp0[i][j] = (dp0[i - 1][j] + dp1[i - 1][j]) % MOD;
                 if (i > limit) {
-                    // Subtract the case where we had 'limit' zeros already and added one more.
-                    // This invalid case is specifically when we had a '1' followed by exactly 'limit' zeros.
-                    dp[i][j][0] = (dp[i][j][0] - dp[i - limit - 1][j][1] + MOD) % MOD;
+                    dp0[i][j] = (dp0[i][j] - dp1[i - limit - 1][j] + MOD) % MOD;
                 }
 
-                // To end with a 1 at (i, j):
-                dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
+                // Transition for ending in 1
+                dp1[i][j] = (dp0[i][j - 1] + dp1[i][j - 1]) % MOD;
                 if (j > limit) {
-                    dp[i][j][1] = (dp[i][j][1] - dp[i][j - limit - 1][0] + MOD) % MOD;
+                    dp1[i][j] = (dp1[i][j] - dp0[i][j - limit - 1] + MOD) % MOD;
                 }
             }
         }
 
-        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+        return (dp0[zero][one] + dp1[zero][one]) % MOD;
     }
 };
