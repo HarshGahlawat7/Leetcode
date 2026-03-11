885d99e2597e92bd158dd609e0e449f58105ee21
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-12 00:22:08 +0530

    Time: 0 ms (100%), Space: 8 MB (26.22%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+
+        // Find the number of bits in n
+        // For n = 5 (101), bits = 3
+        unsigned int num_bits = 0;
+        int temp = n;
+        while (temp > 0) {
+            temp >>= 1;
+            num_bits++;
+        }
+
+        // Create a mask of all 1s of length num_bits
+        // (1U << 3) is 1000, subtracting 1 gives 0111
+        unsigned int mask = (1U << num_bits) - 1;
+
+        // XOR n with the mask to flip all bits up to the MSB
+        return n ^ mask;
+    }
+};
