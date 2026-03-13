4ff52f156de87f6afdfd4dae353fd0977e870f89
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-13 21:11:20 +0530

    Time: 3 ms (96.77%), Space: 29 MB (65.81%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
         long long low = 0;
-        // Upper bound: max workerTime (10^6) * H * (H+1) / 2 where H = 10^5
-        // 1e16 is a safe ceiling for a 64-bit long long.
         long long high = 1e16; 
         long long result = high;
 
@@ -23,8 +21,6 @@ private:
     bool canReduce(int mountainHeight, const vector<int>& workerTimes, long long T) {
         long long totalReduction = 0;
         for (int w : workerTimes) {
-            // Calculate max x such that w * x * (x + 1) / 2 <= T
-            // x = (-1 + sqrt(1 + 8*T/w)) / 2
             double val = 1.0 + (8.0 * T) / w;
             long long x = (sqrt(val) - 1) / 2;
             
