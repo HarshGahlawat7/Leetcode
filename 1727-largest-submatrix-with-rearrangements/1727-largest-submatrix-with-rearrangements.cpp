2044519236e180bcb6db0ee2bf97efd439c37964
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-18 00:19:37 +0530

    Time: 49 ms (29.26%), Space: 89.3 MB (61.7%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -7,18 +7,14 @@ public:
 
         for (int r = 0; r < m; ++r) {
             for (int c = 0; c < n; ++c) {
-                // If current cell is 1, accumulate height from above
                 if (matrix[r][c] != 0 && r > 0) {
                     matrix[r][c] += matrix[r - 1][c];
                 }
             }
 
-            // We need a copy of the row to sort it without 
-            // breaking the height calculations for the next row.
             std::vector<int> currRow = matrix[r];
             std::sort(currRow.begin(), currRow.end(), std::greater<int>());
 
-            // Calculate max area for the current row's heights
             for (int i = 0; i < n; ++i) {
                 int height = currRow[i];
                 int width = i + 1;
