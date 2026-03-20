b1b33bfd9e8258def15f6096a4a29deceece3a5f
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-03-21 00:43:22 +0530

    Time: 57 ms (34.07%), Space: 47 MB (22.96%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -4,7 +4,6 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        // Result dimensions: (m - k + 1) x (n - k + 1)
         int res_rows = m - k + 1;
         int res_cols = n - k + 1;
         vector<vector<int>> ans(res_rows, vector<int>(res_cols));
@@ -12,7 +11,6 @@ public:
         for (int i = 0; i < res_rows; ++i) {
             for (int j = 0; j < res_cols; ++j) {
                 
-                // Use a set to store distinct values in sorted order
                 set<int> distinct_elements;
                 for (int r = i; r < i + k; ++r) {
                     for (int c = j; c < j + k; ++c) {
@@ -20,13 +18,10 @@ public:
                     }
                 }
 
-                // If fewer than 2 distinct elements exist, the diff is 0
                 if (distinct_elements.size() < 2) {
                     ans[i][j] = 0;
                 } else {
                     int min_diff = INT_MAX;
-                    
-                    // Iterate through the sorted set to find the smallest gap
                     auto it = distinct_elements.begin();
                     int prev = *it;
                     ++it;
