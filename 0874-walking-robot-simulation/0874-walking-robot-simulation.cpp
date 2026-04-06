efb1589be3ed2368c69bc895b8cf56fd9d77c093
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-04-06 18:58:23 +0530

    Time: 19 ms (83.47%), Space: 37.9 MB (56.53%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -4,23 +4,21 @@ public:
         int dx[] = {0, 1, 0, -1};
         int dy[] = {1, 0, -1, 0};
         
-        // Store obstacles in a set for fast lookup
-        // We can pack (x, y) into a single long long to use with std::set
         std::set<std::pair<int, int>> obstacleSet;
         for (const auto& obs : obstacles) {
             obstacleSet.insert({obs[0], obs[1]});
         }
         
-        int x = 0, y = 0; // Starting position
-        int dir = 0;      // Starting direction (North)
+        int x = 0, y = 0; 
+        int dir = 0;     
         int maxDistSq = 0;
         
         for (int cmd : commands) {
-            if (cmd == -2) { // Turn left
+            if (cmd == -2) { 
                 dir = (dir + 3) % 4;
-            } else if (cmd == -1) { // Turn right
+            } else if (cmd == -1) { 
                 dir = (dir + 1) % 4;
-            } else { // Move forward k units
+            } else { 
                 for (int i = 0; i < cmd; ++i) {
                     int nextX = x + dx[dir];
                     int nextY = y + dy[dir];
