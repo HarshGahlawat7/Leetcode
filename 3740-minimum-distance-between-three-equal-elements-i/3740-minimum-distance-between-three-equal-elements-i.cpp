581c17917bf73246c07f6db9d2f782909c7b0092
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-04-10 23:36:32 +0530

    Time: 5 ms (43.49%), Space: 29.6 MB (47.69%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
         int n = nums.size();
-        // Map to store the indices of each unique number
         unordered_map<int, vector<int>> indexMap;
         
         for (int i = 0; i < n; ++i) {
@@ -13,14 +12,11 @@ public:
         bool found = false;
         
         for (auto const& [val, indices] : indexMap) {
-            // A good tuple requires at least 3 occurrences
             if (indices.size() >= 3) {
                 found = true;
-                // Check every set of 3 consecutive indices
                 for (int m = 0; m <= (int)indices.size() - 3; ++m) {
                     int i = indices[m];
                     int k = indices[m + 2];
-                    // Distance is 2 * (k - i)
                     int current_dist = 2 * (k - i);
                     min_dist = min(min_dist, current_dist);
                 }
