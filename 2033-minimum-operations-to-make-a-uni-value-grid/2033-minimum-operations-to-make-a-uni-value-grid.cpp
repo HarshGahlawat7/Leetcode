0cb68e5465720e3030520b0242e99e8d3c7a1bc1
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-04-28 22:03:21 +0530

    Time: 47 ms (35.8%), Space: 90 MB (53.91%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,15 +4,12 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<int> nums;
-        
-        // Flatten the grid into a 1D vector
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 nums.push_back(grid[i][j]);
             }
         }
-        
-        // All elements must have the same remainder modulo x
+       
         int remainder = nums[0] % x;
         for (int val : nums) {
             if (val % x != remainder) {
@@ -20,13 +17,10 @@ public:
             }
         }
         
-        // Sort to find the median
         sort(nums.begin(), nums.end());
         
         int median = nums[nums.size() / 2];
         int operations = 0;
-        
-        // Sum the steps required to reach the median
         for (int val : nums) {
             operations += abs(val - median) / x;
         }
