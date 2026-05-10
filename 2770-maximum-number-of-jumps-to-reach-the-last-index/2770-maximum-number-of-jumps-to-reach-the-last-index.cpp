6878941c198d1c090ef13849f669e8f45fe12127
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-05-10 14:32:44 +0530

    Time: 31 ms (16.15%), Space: 68.4 MB (84.78%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
-        // Initialize dp array with -1 to represent unreachable indices
         std::vector<int> dp(n, -1);
         dp[0] = 0;
 
         for (int j = 1; j < n; ++j) {
             for (int i = 0; i < j; ++i) {
-                // Check if the previous index i is reachable
                 if (dp[i] != -1) {
-                    // Use long long for the difference to prevent potential overflow
                     long long diff = (long long)nums[j] - nums[i];
                     
                     if (diff >= -target && diff <= target) {
