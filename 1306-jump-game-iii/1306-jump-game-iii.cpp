e9f4c310ec97930d44e838d583424d0ba3e4acad
Harsh Gahlawat <gahlawatharsh69@gmail.com>
2026-05-18 00:21:51 +0530

    Time: 0 ms (100%), Space: 35.3 MB (85.6%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        std::vector<bool> visited(n, false);
+        std::queue<int> q;
+        
+        q.push(start);
+        visited[start] = true;
+        
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            
+            // If we found a target index with value 0
+            if (arr[curr] == 0) {
+                return true;
+            }
+            
+            // Option 1: Jump forward
+            int forward = curr + arr[curr];
+            if (forward < n && !visited[forward]) {
+                visited[forward] = true;
+                q.push(forward);
+            }
+            
+            // Option 2: Jump backward
+            int backward = curr - arr[curr];
+            if (backward >= 0 && !visited[backward]) {
+                visited[backward] = true;
+                q.push(backward);
+            }
+        }
+        
+        return false;
+    }
+};
